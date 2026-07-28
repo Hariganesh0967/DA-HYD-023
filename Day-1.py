ec4dcb9705a8f8c583bd9e59412c0013777c9703
Hariganesh0967 <ganeshganthala123@gmail.com>
2026-07-28 15:09:21 +0530

    day-1

---
+++ Day-1.py
@@ -0,0 +1,44 @@
+#IDLE is a colour Environment
+#hastag isnused as single line comment...
+#we use triple quote(single/double) ''',"""as multiline comments
+print('Hello World')
+
+'''
+tokens--> a smallest unit in a program,there are 5 typre of token.
+
+->keywords
+->identifiers
+->Literals
+->Operators
+->Punctuators
+'''
+#keywords -->these are reservred words in python .we have 35 keywords...
+#help() -->keywords -->it gives list of 35 keywords -->true ,false.....
+
+#identifiers -->these are the names being alloted to variables,function,classes...
+#we cannot start with a number.
+#we can start with uppercae /lowercase letters (A-Z)
+name = 'Hari'
+print(name)
+email_id = 'ganeshganthala123@gmail.com'
+print(email_id)
+#prinyt(Email_id #python is case_sensitive -->NameError
+#1swer = 23
+#print(1swer)
+#@derrrr=3.4 #No special character
+
+#True=45 # No keywords
+
+batch="da23"
+print(batch)
+
+#literal -->These are constants (value(s)) -->numbetr,characters
+
+#Operators -->Operators perform spevcific operators -->7types
+
+length = 5
+breadth = 3
+area = length*breadth
+print(area)
+
+
